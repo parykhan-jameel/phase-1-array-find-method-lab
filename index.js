9a04800b704e1150fd9878ff294084ceff211453
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 14:13:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array){
+    let foundItem
+    if (foundItem = array.find(array =>array.result==='W')){
+      return foundItem.year
+      } else return undefined
+  }
